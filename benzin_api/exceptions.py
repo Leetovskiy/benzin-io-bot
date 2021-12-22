b3bb920eb0783256ee4ee03d82c55e1bc0f15a2b
Vitaliy Zaitsev <a.leetovskiy@gmail.com>
2021-12-22 18:48:33 +0500

    Recycle exceptions.py
    The custom exception call has been replaced by the raise_for_status method built into aiohttp because the previous solution was wrong. The unused MissingImage exception was removed as unnecessary.

--- benzin_api/exceptions.py
+++ benzin_api/exceptions.py
@@ -12,17 +12,6 @@ class BenzinException(BaseException):
         super().__init__(self.message)
 
 
-class MissingImage(BenzinException):
-    """Missing image error"""
-
-    def __init__(self, message: str = None):
-        if message is None:
-            message = 'Input image is not specified in HTTP-request'
-
-        self.message = message
-        super().__init__(self.message)
-
-
 class UnspecifiedToken(BenzinException):
     """Token is not specified"""
 
