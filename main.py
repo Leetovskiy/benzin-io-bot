48b47922cf5187f342a0948db03a267cf6d50bd6
Vitaliy Zaitsev <a.leetovskiy@gmail.com>
2022-01-04 00:01:00 +0500

    Replace aiogram logging with its own

--- main.py
+++ main.py
@@ -3,12 +3,13 @@
 from aiogram import executor
 from loguru import logger
 
+# noinspection PyUnresolvedReferences
 import handlers
 from loader import dp
 
 if __name__ == '__main__':
     try:
-        logger.info('Bot start')
+        logger.info('Bot has started')
         executor.start_polling(dispatcher=dp, skip_updates=True)
     finally:
-        logger.info('Bot stopped')
+        logger.info('Bot has stopped')
