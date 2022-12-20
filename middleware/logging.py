61a2b3e2b23332d65d901e723821ef1b89413fce
Vitaliy Leetovskiy <a.leetovskiy@gmail.com>
2022-12-21 00:05:52 +0500

    Add the copyright notice

--- middleware/logging.py
+++ middleware/logging.py
@@ -1,3 +1,6 @@
+# Copyright © 2022 Vitaliy Zaitsev. All rights reserved.
+# Contacts: dev.zaitsev@gmail.com
+# Licensed under the Apache License, Version 2.0
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.types import Message
 from loguru import logger
